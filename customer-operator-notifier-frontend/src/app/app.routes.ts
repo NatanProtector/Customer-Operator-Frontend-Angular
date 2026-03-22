98064cd1d5f88dcb9e4552d202cea65d3dc949c4
Natan <155454141+NatanProtector@users.noreply.github.com>
2026-03-22 07:52:04 +0200

    Removed all default style and default page. basic call to DB to get the

--- customer-operator-notifier-frontend/src/app/app.routes.ts
+++ customer-operator-notifier-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DatabasePageComponent } from './pages/database-page.component';
+import { NotificationsPageComponent } from './pages/notifications.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'database', pathMatch: 'full' },
+	{ path: 'database', component: DatabasePageComponent },
+	{ path: 'notifications', component: NotificationsPageComponent }
+];
---
+++ customer-operator-notifier-frontend/src/app/pages/notifications.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-page',
+  standalone: true,
+  template: '<h1>Notifications</h1>'
+})
+export class NotificationsPageComponent {}
